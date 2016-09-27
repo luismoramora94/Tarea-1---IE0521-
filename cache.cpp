49db63639d46a58501809c6d501986b703c7649e
luismoramora94 <lmoramora94@gmail.com>
2016-09-27 11:47:29 -0600

    Add files via upload

---
+++ cache.cpp
@@ -0,0 +1,24 @@
+#include "cache.h"
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+Cache::Cache():{
+};
+
+Cache::Cache(int n){
+
+
+}
+
+Cache::Cache(const Cache& other):{
+
+}
+
+Cache::~Cache(){
+};
+
+void Cache::push();
+void Cache::pop();
